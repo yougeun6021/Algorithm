dbcb9e7e7b7e2998246c4c086e8f56c160822920
yougeun6021 <dbrms1a1a@naver.com>
2022-10-06 21:00:17 +0900

    10월 6일 알고리즘

---
+++ src/Level1/October6/NumberStringAndWord.java
@@ -0,0 +1,17 @@
+package Level1.October6;
+
+public class NumberStringAndWord { //숫자 문자열과 영단어
+    public static void main(String[] args) {
+        String s ="one4seveneight";
+        System.out.println(solution(s));
+    }
+
+    public static int solution(String s) {
+        String[] number = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        for(int i=0; i< number.length; i++){
+            s = s.replaceAll(number[i],Integer.toString(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
