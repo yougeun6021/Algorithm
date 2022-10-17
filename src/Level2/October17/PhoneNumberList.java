98905ef2aee740714bed1107846a6115cc79fd99
yougeun6021 <dbrms1a1a@naver.com>
2022-10-17 19:19:02 +0900

    10월 17일 알고리즘

---
+++ src/Level2/October17/PhoneNumberList.java
@@ -0,0 +1,21 @@
+package Level2.October17;
+
+import java.util.Arrays;
+
+public class PhoneNumberList { //전화번호 목록
+    public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+        System.out.println(solution(phone_book));
+
+    }
+
+    public static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for(int i=0; i<phone_book.length-1;i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
