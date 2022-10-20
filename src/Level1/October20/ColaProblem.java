78cb9781614ed1f74ae4159fecac207b94d82191
yougeun6021 <dbrms1a1a@naver.com>
2022-10-20 18:05:47 +0900

    10월 20일 알고리즘

---
+++ src/Level1/October20/ColaProblem.java
@@ -0,0 +1,20 @@
+package Level1.October20;
+
+public class ColaProblem { //콜라 문제
+    public static void main(String[] args) {
+        int a =2;
+        int b =1;
+        int n =20;
+        System.out.println(solution(a,b,n));
+
+    }
+
+    public static int solution(int a, int b, int n) {
+        int answer = 0;
+        while((n/a>=1)){
+            answer = answer+b*(n/a);
+            n = b*(n/a)+n%a;
+        }
+        return answer;
+    }
+}
