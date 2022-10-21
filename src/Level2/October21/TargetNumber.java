7de9199da3bfff0b96e2c9c946141859f12cd5f5
yougeun6021 <dbrms1a1a@naver.com>
2022-10-21 18:39:37 +0900

    10월 21일 알고리즘

---
+++ src/Level2/October21/TargetNumber.java
@@ -0,0 +1,31 @@
+package Level2.October21;
+
+
+public class TargetNumber { //타겟 넘버
+    public static void main(String[] args) {
+        int[] numbers = {1, 1, 1, 1, 1};
+        int target = 3;
+        System.out.println(solution(numbers,target));
+
+    }
+
+    public static int  solution(int[] numbers, int target) {
+        return dfs(numbers, target, 0, 0);
+    }
+
+    public static int dfs(int[] numbers, int target, int depth, int sum) {
+        int targetCount = 0;
+
+        if (depth == numbers.length) {
+            if (sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+
+        targetCount += dfs(numbers, target, depth + 1, sum + numbers[depth]);
+        targetCount += dfs(numbers, target, depth + 1, sum - numbers[depth]);
+        return targetCount;
+    }
+
+}
