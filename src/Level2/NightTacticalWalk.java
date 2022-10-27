ee829e947666b331ea49a2df26adf6fb308afef1
yougeun6021 <dbrms1a1a@naver.com>
2022-10-27 16:48:09 +0900

    10월 27일 알고리즘

---
+++ src/Level2/NightTacticalWalk.java
@@ -0,0 +1,35 @@
+package Level2;
+
+
+
+public class NightTacticalWalk { //야간전술보행
+    public static void main(String[] args) {
+        int distance =12;
+        int[][] scope = {{3, 5},{10, 11}};
+        int[][] times = {{3, 5},{2, 3}};
+        System.out.println(solution(distance,scope,times));
+
+    }
+
+    public static int solution(int distance, int[][] scope, int[][] times) {
+        int answer = Integer.MAX_VALUE;
+        for(int i=0; i<scope.length;i++){
+            int scopeMin = Math.min(scope[i][0],scope[i][1]);
+            int scopeMax =Math.max(scope[i][0],scope[i][1]);
+            for(int j= scopeMin; j<=scopeMax; j++){
+                int period = times[i][0]+times[i][1];
+                if(j%period<=times[i][0] && j%period>=1 ){
+                    if(answer>j){
+                        answer =j;
+                    }
+                }
+            }
+        }
+
+        if(answer==Integer.MAX_VALUE){
+            return distance;
+        }
+
+        return answer;
+    }
+}
