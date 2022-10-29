d6f00f759deb745b6e004d430d66d38c4966cd86
yougeun6021 <dbrms1a1a@naver.com>
2022-10-29 13:30:20 +0900

    10월 29일 알고리즘

---
+++ src/Level1/October29/SumConsecutiveNumbers.java
@@ -0,0 +1,23 @@
+package Level1.October29;
+
+import java.util.Arrays;
+
+public class SumConsecutiveNumbers { //연속된 수들의 합
+    public static void main(String[] args) {
+        int num = 3;
+        int total =12;
+        System.out.println(Arrays.toString(solution(num, total)));
+
+
+    }
+
+    public static int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int startInteger = total/num-(num-1)/2;
+        int index =0;
+        for(int i=startInteger;i<startInteger+num;i++){
+            answer[index++] = i;
+        }
+        return answer;
+    }
+}
