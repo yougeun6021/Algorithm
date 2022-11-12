1bc9ed0c0ead8baec301510c1a98831414fdcad3
yougeun6021 <dbrms1a1a@naver.com>
2022-11-12 16:26:56 +0900

    11월 12일 알고리즘

---
+++ src/Level2/November12/N_notationGame.java
@@ -0,0 +1,29 @@
+package Level2.November12;
+
+public class N_notationGame { // N 진수 게임
+    public static void main(String[] args) {
+        int n = 16;
+        int t = 16;
+        int m = 2;
+        int p = 1;
+        System.out.println(solution(n,t,m,p));
+
+    }
+
+    public static String solution(int n, int t, int m, int p) {
+        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        int number =0;
+
+        while (sb.length() <t*m){
+            sb.append(Integer.toString(number++,n));
+        }
+
+
+        for(int i=p-1; i<p-1+t*m;i=i+m){
+                answer.append(sb.charAt(i));
+        }
+
+        return answer.toString().toUpperCase();
+    }
+}
---
+++ src/Level2/November12/StockPrice.java
@@ -0,0 +1,35 @@
+package Level2.November12;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StockPrice { //주식가격
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 2, 3};
+        System.out.println(Arrays.toString(solution(prices)));
+
+    }
+
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Queue<Integer> queue = new LinkedList<>();
+        int index = 0;
+        int time = 0;
+        for(int price: prices){
+            queue.add(price);
+        }
+        while (!queue.isEmpty()){
+            int price = queue.poll();
+            for(Integer p:queue){
+                time++;
+                if(p<price){
+                    break;
+                }
+            }
+            answer[index++] = time;
+            time =0;
+        }
+        return answer;
+    }
+}
