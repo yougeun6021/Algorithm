d8dceb2f10597f5d1baea28e824a4a04a740a7c6
yougeun6021 <dbrms1a1a@naver.com>
2022-11-25 18:25:07 +0900

    효율적인 약수개수 알고리즘

---
+++ src/Algorithm/PrimeNumber.java
@@ -0,0 +1,108 @@
+package Algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int e =5000000;
+        int[] divisorArr = new int[e+1];
+        int[] divisorArr2 = new int[e+1];
+        long beforeTime = System.currentTimeMillis();
+        getDivisorArr(e,divisorArr);
+        long afterTime = System.currentTimeMillis();
+        System.out.println(afterTime-beforeTime);
+
+
+        beforeTime = System.currentTimeMillis();
+        getDivisorArr2(e,divisorArr2);
+        afterTime = System.currentTimeMillis();
+        System.out.println(afterTime-beforeTime);
+    }
+
+    public static void getDivisorArr(int e,int[] divisorArr){
+        for(int i=1; i<=e;i++){
+            divisorArr[i] = getDivisorNumber2(i);
+        }
+    }
+
+
+    public static int getDivisorNumber(int n){
+        int count =0;
+        double sqrt = Math.sqrt(n);
+        if(sqrt%1==0){
+            count++;
+            if(n==1){
+                return count;
+            }
+        }
+
+        for(int i=1; i<sqrt;i++){
+            if(n%i==0){
+                count+=2;
+            }
+        }
+
+        return count;
+    }
+
+
+
+    public static void getDivisorArr2(int e,int[] divisorNumber){
+        List<Integer> primeList = new ArrayList<>();
+        for(int i=1; i<=e;i++){
+            if(divisorNumber[i]==0){
+                divisorNumber[i] = getDivisorNumber2(i);
+                primeList.add(i);
+            }
+            for (Integer prime : primeList) {
+                if(i*prime<e){
+                    if(divisorNumber[i*prime]!=0){
+                        continue;
+                    }
+                    if(i%prime==0){
+                        int num = i;
+                        int count = 0;
+                        while(num%prime==0){
+                            num = num/prime;
+                            count++;
+                        }
+                        divisorNumber[i * prime] = divisorNumber[i]/(count+1)*(count+2);
+                    }else{
+                        divisorNumber[i * prime] = divisorNumber[i] * 2;
+                    }
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+
+    public static int getDivisorNumber2(int n){
+        int count =0;
+        double sqrt = Math.sqrt(n);
+        if(sqrt%1==0){
+            count++;
+            if(n==1){
+                return count;
+            }
+        }
+        if(n%2==0){
+            for(int i=1; i<sqrt;i++){
+                if(n%i==0){
+                    count+=2;
+                }
+            }
+        }else{
+            for(int i=1; i<sqrt;i=i+2){
+                if(n%i==0){
+                    count+=2;
+                }
+            }
+        }
+        return count;
+    }
+
+
+
+}
--- src/Level3/Memorize.java
+++ src/Level3/Memorize.java
@@ -6,23 +6,21 @@ public class Memorize { //억억단을 외우자 ->테스트 10 시간초과
     public static void main(String[] args) {
         int e =8;
         int[] starts = {1,3,7};
-        System.out.println(Arrays.toString(solution2(e, starts)));
-
+        System.out.println(Arrays.toString(solution3(e, starts)));
     }
 
     public static int[] solution(int e, int[] starts) {
         int[] answer = new int[starts.length];
-        int[] divisorArr = new int[e];
+        int[] divisorArr = new int[e+1];
+
+        getDivisorArr2(e,divisorArr);
+
         int index = 0;
         for(int i=0; i<starts.length;i++){
             int max = 0;
             for(int j=starts[i];j<=e;j++){
 
-                if(divisorArr[j-1]==0){
-                    getDivisorNumber(j,divisorArr);
-                }
-
-                int value = divisorArr[j-1];
+                int value = divisorArr[j];
 
                 if(max<value){
                     index = j;
@@ -59,24 +57,34 @@ public class Memorize { //억억단을 외우자 ->테스트 10 시간초과
         return answer;
     }
 
+    public static int[] solution3(int e, int[] starts) {
+        int[] answer = new int[starts.length];
+        int[] divisorArr = new int[e+1];
+        TreeSet<Number> set = new TreeSet<>(Comparator.reverseOrder());
 
-    public static void getDivisorNumber(int n,int[] divisorArr){
-        int count =0;
-        double sqrt = Math.sqrt(n);
-        if(sqrt%1==0){
-            count++;
-            if(n==1){
-                divisorArr[n-1] = count;
-                return;
+        getDivisorArr3(e,divisorArr,set);
+
+
+        for(int i=0; i< starts.length;i++){
+            for(Number number:set){
+                int num = number.getNumber();
+                if(num>=starts[i]){
+                    answer[i] = num;
+                    break;
+                }
             }
         }
-        for(int i=1; i<sqrt;i++){
-            if(n%i==0){
-                count+=2;
-            }
-        }
-        divisorArr[n-1] = count;
+
+        return answer;
     }
+
+
+    public static void getDivisorArr(int e,int[] divisorArr){
+        for(int i=1; i<=e;i++){
+            divisorArr[i] = getDivisorNumber2(i);
+        }
+    }
+
 
     public static int getDivisorNumber(int n){
         int count =0;
@@ -87,13 +95,106 @@ public class Memorize { //억억단을 외우자 ->테스트 10 시간초과
                 return count;
             }
         }
+
         for(int i=1; i<sqrt;i++){
             if(n%i==0){
                 count+=2;
             }
         }
+
         return count;
     }
+
+
+
+    public static void getDivisorArr2(int e,int[] divisorNumber){
+        List<Integer> primeList = new ArrayList<>();
+        for(int i=1; i<=e;i++){
+            if(divisorNumber[i]==0){
+                divisorNumber[i] = getDivisorNumber2(i);
+                primeList.add(i);
+            }
+            for (Integer prime : primeList) {
+                if(i*prime<e){
+                    if(divisorNumber[i*prime]!=0){
+                        continue;
+                    }
+                    if(i%prime==0){
+                        int num = i;
+                        int count = 0;
+                        while(num%prime==0){
+                            num = num/prime;
+                            count++;
+                        }
+                        divisorNumber[i * prime] = divisorNumber[i]/(count+1)*(count+2);
+                    }else{
+                        divisorNumber[i * prime] = divisorNumber[i] * 2;
+                    }
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+
+    public static int getDivisorNumber2(int n){
+        int count =0;
+        double sqrt = Math.sqrt(n);
+        if(sqrt%1==0){
+            count++;
+            if(n==1){
+                return count;
+            }
+        }
+        if(n%2==0){
+            for(int i=1; i<sqrt;i++){
+                if(n%i==0){
+                    count+=2;
+                }
+            }
+        }else{
+            for(int i=1; i<sqrt;i=i+2){
+                if(n%i==0){
+                    count+=2;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void getDivisorArr3(int e,int[] divisorNumber,TreeSet<Number> set){
+        List<Integer> primeList = new ArrayList<>();
+        for(int i=1; i<=e;i++){
+            if(divisorNumber[i]==0){
+                divisorNumber[i] = getDivisorNumber2(i);
+                primeList.add(i);
+                set.add(new Number(i,divisorNumber[i]));
+            }
+            for (Integer prime : primeList) {
+                if(i*prime<e){
+                    if(divisorNumber[i*prime]!=0){
+                        continue;
+                    }
+                    if(i%prime==0){
+                        int num = i;
+                        int count = 0;
+                        while(num%prime==0){
+                            num = num/prime;
+                            count++;
+                        }
+                        divisorNumber[i * prime] = divisorNumber[i]/(count+1)*(count+2);
+                    }else{
+                        divisorNumber[i * prime] = divisorNumber[i] * 2;
+                    }
+                    set.add(new Number(i*prime,divisorNumber[i*prime]));
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+
+
 
     static class Number implements Comparable<Number> {
         int number;
